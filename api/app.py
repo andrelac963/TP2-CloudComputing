767346b2f453071b6a49d31656edc8e075a4a2f3
Bernnardo Seraphim <71301567+BernnardoSBO@users.noreply.github.com>
2023-11-28 03:58:28 -0300

    Update app.py

--- api/app.py
+++ api/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify, make_response
 from flask_cors import CORS
 import pickle
 
-app = Flask(__name__) 
+app = Flask(__name__)  
 
 CORS(app)
 CORS(app, resources={r"/api/*": {"origins": "*"}})
